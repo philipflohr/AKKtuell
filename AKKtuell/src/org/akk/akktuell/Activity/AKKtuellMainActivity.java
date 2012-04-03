c4a5cc15e06a26e8f4745867d066151fcf1ca529
AkkPan <app@akk.org>
2012-04-04 00:17:31 +0200

    using simleCursorAdapter to fetch Data from DB and display to list; mock function added to InfoManager. Has to be replaced; added id to Listview_Layout;

--- AKKtuell/src/org/akk/akktuell/Activity/AKKtuellMainActivity.java
+++ AKKtuell/src/org/akk/akktuell/Activity/AKKtuellMainActivity.java
@@ -4,19 +4,43 @@ import org.akk.akktuell.R;
 import org.akk.akktuell.Model.InfoManager;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class AKKtuellMainActivity extends Activity {
 	
 	private InfoManager infoManager;
+	private ListView elementListView;
 	
 	
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         infoManager = new InfoManager();
+
+        elementListView = (ListView) findViewById(R.id.main_element_listview);
+        
+        //check online
+        
+        displayData();
+        
         setContentView(R.layout.main);
+    }
+    
+    private void displayData() {
+    	Cursor cursor = infoManager.getData();
+    	
+    	//TODO define fields from db to display
+    	String[] elementIDs = new String[] {"Date", "Title"};
+    	
+    	//set View to use
+    	int[] itemView = new int[] { R.id.listitem_eventdate, R.id.listitem_eventname};
+    	
+    	SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.main_activity_list_item, cursor, elementIDs, itemView);
+    	
+    	elementListView.setAdapter(adapter);
     }
 }
