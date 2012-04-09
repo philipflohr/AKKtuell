85404cceae44d09b96ed2f0f344ed1440be56303
Philip Flohr <philip.flohr@student.kit.edu>
2012-04-09 09:39:58 +0200

    added gesture support in mainActivity

--- AKKtuell/src/org/akk/akktuell/Activity/AKKtuellMainActivity.java
+++ AKKtuell/src/org/akk/akktuell/Activity/AKKtuellMainActivity.java
@@ -7,21 +7,42 @@ import org.akk.akktuell.Model.InfoManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class AKKtuellMainActivity extends Activity {
+public class AKKtuellMainActivity extends Activity  {
 	
 	private InfoManager infoManager;
 	private ListView elementListView;
+	private GestureDetector gestureScanner;
+	private int monthCounter;
+	private static int MAX_SIZE_OF_GESTURE=900;
 	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        monthCounter = 0;
+        gestureScanner = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {			
+			@Override
+			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+					float velocityY) {
+				if (velocityX < -MAX_SIZE_OF_GESTURE) {
+					monthCounter++;
+				} else if (velocityX > MAX_SIZE_OF_GESTURE){
+					monthCounter--;
+				} else {
+					//this is not a guesture we want to interpret
+				}
+				AKKtuellMainActivity.this.displayData();
+				return false;
+			}
+		});
         infoManager = new InfoManager(getApplicationContext());
         
         setContentView(R.layout.main);
@@ -48,11 +69,23 @@ public class AKKtuellMainActivity extends Activity {
     private void displayData() {
     	if (!infoManager.readyToDisplayData()) {
     		//wait for data update
-    	} 
+    	}
+		View mainView = findViewById(R.id.main_activity_layout);
+		TextView listHeaderMonthName = (TextView) mainView.findViewById(R.id.main_activity_list_header);
+		listHeaderMonthName.setText("TestHeader" + monthCounter);
     	AkkEventAdapter adapter = new AkkEventAdapter(getApplicationContext(), infoManager.getEvents(), infoManager);
     	elementListView.setAdapter(adapter);
     	
-    }
+    	
+    } 
     
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+	    if (gestureScanner != null) {
+	        if (gestureScanner.onTouchEvent(ev))
+	            return true;
+	    }
+	    return super.dispatchTouchEvent(ev);
+	}
     
 }
